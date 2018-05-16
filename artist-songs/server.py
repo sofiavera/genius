ad45cf54c188505eff5bd8a27357cbde33ad676f
s.vera.2017 <spamdesofia@gmail.com>
2018-05-16 12:58:54 +0200

    amoacentrarnos

--- artist-songs/server.py
+++ artist-songs/server.py
@@ -8,23 +8,79 @@ socketserver.TCPServer.allow_reuse_address = True
 IP = "localhost"
 PORT = 8000
 
-api_token = sys.argv[1]
+TOKEN = sys.argv[1]
 
-class Client():
-    def get_url(self,choice):
-        headers = {"Authorization": "Bearer " + self.api_token}
+class Genius_Client():
+    def get_singer(self, choice):
+        headers = {"Authorization": "Bearer " + TOKEN }
 
         conn = http.client.HTTPSConnection("api.genius.com")
-        url =
+        url = "/search?q=" + choice
         conn.request("GET", url , None, headers)
-        print('amoaver', url)
         r1 = conn.getresponse()
         print(r1.status, r1.reason)
         res_raw = r1.read().decode("utf-8")
         conn.close()
 
-        search = json.loads(res_raw)
-        return search
+        singer = json.loads(res_raw)
+        return singer
+    def get_id(self, singer):
+        try:
+            for i in singer['response']['hits']:
+                id = i['result']['primary_artist']['id']
+                break
+        except KeyError:
+            with open("not_found.html") as f:
+                message = f.read()
+            self.wfile.write(bytes(message, "utf8"))
+
+        return id
+    def get_url(self, id):
+        headers = {"Authorization": "Bearer " + TOKEN}
+
+        conn = http.client.HTTPSConnection("api.genius.com")
+        url = "/artists/" + id + "/songs?per_page=30&page=1"
+        conn.request("GET", url, None, headers)
+        r1 = conn.getresponse()
+        print(r1.status, r1.reason)
+        res_raw = r1.read().decode("utf-8")
+        conn.close()
+        data = json.loads(res_raw)
+        return data
+
+client = Genius_Client()
+
+class Genius_Parser():
+    def get_data(self,data):
+        list = []
+        try:
+            list = data['response']['songs']
+        except KeyError:
+            with open("not_found.html") as f:
+                message = f.read()
+            self.wfile.write(bytes(message, "utf8"))
+
+        return list
+class Genius_HTML():
+    def write_data(self, list):
+        html_file = "<body style='background-color:#ffff64;'>"
+        html_file += "<font face = ""Arial"">"
+        html_file += "<a href=""http://localhost:8000/"">Back to Main Page</a></p>"
+        html_file += '</head><body><h1>Songs found of the requested singer/band</h1>'
+        for song in song_list:
+            html_file += "<li>"
+            if song['header_image_thumbnail_url'].find('default cover'):
+                html_file += "<img align='left' height='50' width='50' src=' " + song[
+                    'header_image_thumbnail_url'] + "'>"
+            else:
+                html_file += '(Album photo not found)'
+
+            html_file += "<a href='" + song['url'] + "'>" + "<h2>" + song['title'] + "</h2></a></li>"
+        html_file += "</body></html>"
+
+        return html_file
+
+
 
 
 
@@ -45,8 +101,12 @@ class Handler(http.server.BaseHTTPRequestHandler):
                 self.send_response(200)
                 self.send_header('Content-type', 'text/html')
                 self.end_headers()
-                print(self.path)
-                Client().get_url()
+                choice = self.path.split("=")[1]
+                singer = client.get_singer(choice)
+                id = client.get_id(singer)
+                data = client.get_url(id)
+
+
 
         except KeyError:
             self.send_response(404)
