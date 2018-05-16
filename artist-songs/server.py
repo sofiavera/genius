072b32167dee5ca8596c76c14dd18e8fb0362657
s.vera.2017 <spamdesofia@gmail.com>
2018-05-16 11:47:32 +0200

    amoacentrarnos

--- artist-songs/server.py
+++ artist-songs/server.py
@@ -8,6 +8,26 @@ socketserver.TCPServer.allow_reuse_address = True
 IP = "localhost"
 PORT = 8000
 
+api_token = sys.argv[1]
+
+class Client():
+    def get_url(self,choice):
+        headers = {"Authorization": "Bearer " + self.api_token}
+
+        conn = http.client.HTTPSConnection("api.genius.com")
+        url =
+        conn.request("GET", url , None, headers)
+        print('amoaver', url)
+        r1 = conn.getresponse()
+        print(r1.status, r1.reason)
+        res_raw = r1.read().decode("utf-8")
+        conn.close()
+
+        search = json.loads(res_raw)
+        return search
+
+
+
 class Handler(http.server.BaseHTTPRequestHandler):
     def do_GET(self):
         self.send_response(200)
@@ -21,3 +41,29 @@ class Handler(http.server.BaseHTTPRequestHandler):
                 with open("genius.html", "r") as f:
                     menu = f.read()
                     self.wfile.write(bytes(menu, "utf8"))
+            elif "searchSongs" in self.path:
+                self.send_response(200)
+                self.send_header('Content-type', 'text/html')
+                self.end_headers()
+                print(self.path)
+                Client().get_url()
+
+        except KeyError:
+            self.send_response(404)
+            self.send_header('Content-type', 'text/html')
+            self.end_headers()
+            with open("error.html", "r") as f:
+                file = f.read()
+            self.wfile.write(bytes(file, "utf8"))
+
+        return
+
+Handler.api_token = sys.argv[1]
+
+httpd = socketserver.TCPServer(("", PORT), Handler)
+print("serving at port", PORT)
+try:
+    httpd.serve_forever()
+except KeyboardInterrupt:
+    pass
+httpd.server_close()
